05b4d499b93011d95db58c3c5a9aa133809c95cb
drberrones-byte <dr.berrones@gmail.com>
2025-11-29 13:41:12 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,25 +1,26 @@
 const express = require("express");
-const { twiml: { MessagingResponse } } = require("twilio");
-
 const app = express();
-app.use(express.urlencoded({ extended: false }));
 
 const PORT = process.env.PORT || 3000;
 
+// Endpoint para probar que la app está viva
 app.get("/", (req, res) => {
   res.send("Bot de WhatsApp del Dr. Berrones está vivo 😎");
 });
 
+// Endpoint que usaremos después con Twilio
 app.post("/whatsapp", (req, res) => {
-  const twiml = new MessagingResponse();
-
-  twiml.message(
-    "Hola 👋, soy el asistente del Dr. Berrones. " +
-    "Cuéntame brevemente qué te pasa y en breve te contactaremos."
-  );
+  const twiml =
+    ⁠ <?xml version="1.0" encoding="UTF-8"?> ⁠ +
+    ⁠ <Response> ⁠ +
+    ⁠ <Message> ⁠ +
+    `Hola 👋, soy el asistente del Dr. Berrones. ` +
+    ⁠ Cuéntame brevemente qué te pasa y en breve te contactaremos. ⁠ +
+    ⁠ </Message> ⁠ +
+    ⁠ </Response> ⁠;
 
   res.type("text/xml");
-  res.send(twiml.toString());
+  res.send(twiml);
 });
 
 app.listen(PORT, () => {
